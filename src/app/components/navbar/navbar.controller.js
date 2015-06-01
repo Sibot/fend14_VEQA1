be9a726d8d4b922ad48def8e12bd101bee6f9043
TobiasAxelsson <github2.tobis.se>
2015-06-01 21:07:23 +0200

    Initial Yeoman build. Gulp-angular generator.

---
+++ src/app/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('fend14Veqa1')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('fend14Veqa1', ['ngAnimate', 'ngTouch', 'ngSanitize', 'restangular', 'ui.router', 'ngMaterial'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
